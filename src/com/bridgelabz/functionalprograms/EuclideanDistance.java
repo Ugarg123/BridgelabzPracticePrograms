4c94395419aa590ac71a3dc0a923a2226f378f47
unknown <>
2021-12-13 13:09:50 +0530

    ADDED: calculate euclidean distance

--- src/com/bridgelabz/functionalprograms/EuclideanDistance.java
+++ src/com/bridgelabz/functionalprograms/EuclideanDistance.java
@@ -4,4 +4,17 @@ public class EuclideanDistance {
 	static void calculate(int x, int y) {
 		System.out.println("distance is " + Math.pow((x * x + y * y), 0.5));
 	}
+	public static void main(String[] args) {
+		try {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("enter value of x");
+			int x = sc.nextInt();
+			System.out.println("enter value of y");
+			int y = sc.nextInt();
+			calculate(x, y);
+			sc.close();
+		}catch (Exception e) {
+			System.out.println("enter integer only");
+		}
+	}
 }
