5a9ffc8f41bdb29d4aab6631e10c9ebb46a3ae5a
unknown <>
2021-12-13 12:44:57 +0530

    initial commit

---
+++ src/com/bridgelabz/basiccoreprograms/FlipCoin.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class FlipCoin {
+	public static void main(String[] args) {
+		int numOfFlips = 10;
+		int tails = 0;
+		int heads = 0;
+		if(numOfFlips <=0) {
+			System.err.println("Number of flips cant be less than zero");
+		}
+		else {
+			for (int index = 0; index < numOfFlips; index++)
+			{
+				if(Math.random() < 0.5) {
+					tails++;
+				}
+			}
+}
---
+++ src/com/bridgelabz/functionalprograms/EuclideanDistance.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.functionalprograms;
+import java.util.Scanner;
+public class EuclideanDistance {
+	static void calculate(int x, int y) {
+		System.out.println("distance is " + Math.pow((x * x + y * y), 0.5));
+	}
+}
